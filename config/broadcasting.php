c83e2cf7aafbab60d53a5737e7b6ac88003e135a
vimuths123 <vimuths@yahoo.com>
2025-08-17 01:13:47 +0530

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,6 +40,7 @@ return [
                 'port' => env('REVERB_PORT', 443),
                 'scheme' => env('REVERB_SCHEME', 'https'),
                 'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                'path' => env('REVERB_PATH', '/reverb'),
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
